56607e6bcbc292df968542bacb95535d3880e231
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2015-12-03 02:07:37 +0100

    Implement blade (single and all) commands
    * Power button short press
    * Power button long press
    * Reset button press

--- ruggedpod_cli/cli.py
+++ ruggedpod_cli/cli.py
@@ -15,21 +15,75 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import click
+from click import group, option, pass_context, argument, UsageError
 from prettytable import PrettyTable
 
+from client import RuggedPODClient
 
-@click.group()
-@click.option('--api-url', metavar='<url>', envvar='RUGGEDPOD_URL',
-              help='RuggedPOD API base URL (env RUGGEDPOD_URL)', required=True)
-@click.option('--username', metavar='<username>', envvar='RUGGEDPOD_USERNAME',
-              help='RuggedPOD API username (env RUGGEDPOD_USERNAME)', required=True)
-@click.option('--password', metavar='<password>', envvar='RUGGEDPOD_PASSWORD',
-              help='RuggedPOD API password (env RUGGEDPOD_PASSWORD)', required=True)
-@click.pass_context
-def ruggedpod(ctx, api_url, username, password):
+
+@group()
+@option('--api-url', metavar='<url>', envvar='RUGGEDPOD_URL',
+        help='RuggedPOD API base URL (env RUGGEDPOD_URL)', required=True)
+@option('--username', metavar='<username>', envvar='RUGGEDPOD_USERNAME',
+        help='RuggedPOD API username (env RUGGEDPOD_USERNAME)', required=True)
+@option('--password', metavar='<password>', envvar='RUGGEDPOD_PASSWORD',
+        help='RuggedPOD API password (env RUGGEDPOD_PASSWORD)', required=True)
+@option('--debug', is_flag=True, help='Trace HTTP requests and responses')
+@pass_context
+def ruggedpod(ctx, api_url, username, password, debug):
     """RuggedPOD command line interface"""
-    pass
+    ctx.obj['api'] = RuggedPODClient(api_url, username, password, debug)
+
+
+@ruggedpod.command(name='blade-powershort')
+@argument('blade_number', metavar='<blade_number>', required=False)
+@option('--all', is_flag=True, help='Do it on all blades')
+@pass_context
+def power_short_press(ctx, blade_number, all):
+    """Power button short press"""
+    _action(ctx, "Power short press", "SetBladeShortOnOff", "SetAllBladesShortOnOff", blade_number, all)
+
+
+@ruggedpod.command(name='blade-powerlong')
+@argument('blade_number', metavar='<blade_number>', required=False)
+@option('--all', is_flag=True, help='Do it on all blades')
+@pass_context
+def power_long_press(ctx, blade_number, all):
+    """Power button long press"""
+    _action(ctx, "Power long press", "SetBladeLongOnOff", "SetAllBladesLongOnOff", blade_number, all)
+
+
+@ruggedpod.command(name='blade-reset')
+@argument('blade_number', metavar='<blade_number>', required=False)
+@option('--all', is_flag=True, help='Do it on all blades')
+@pass_context
+def reset(ctx, blade_number, all):
+    """Reset button press"""
+    _action(ctx, "Reset", "SetBladeReset", "SetAllBladesReset", blade_number, all)
+
+
+def _action(ctx, label, action, action_all, blade_number, all):
+    if blade_number:
+        _blade_action(ctx, label, action, blade_number)
+    elif all:
+        _all_blade_action(ctx, label, action_all)
+    else:
+        raise UsageError("Either a blade number or the --all option must be provided")
+
+
+def _all_blade_action(ctx, label, action):
+    blades = ctx.obj['api'].action(action)
+    table = PrettyTable(["Blade", "Action", "Status"])
+    for blade in blades:
+        table.add_row([blade['bladeNumber'], label, blade['CompletionCode']])
+    print table
+
+
+def _blade_action(ctx, label, action, blade_number):
+    blade = ctx.obj['api'].action(action, blade_number)
+    table = PrettyTable(["Blade", "Action", "Status"])
+    table.add_row([blade['bladeNumber'], label, blade['CompletionCode']])
+    print table
 
 
 def main():
